7f96a46ab0b7f6932485eb13d004aca153314355
rebecarv <saudeefood@gmail.com>
2025-11-27 11:46:28 -0300

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+// --- CONFIGURAÇÃO DO MAPA ---
+        // 1. Inicializa o mapa nas coordenadas (Latitude, Longitude) e Zoom
+        // Exemplo: Coordenadas de um teatro em SP. Você pode pegar no Google Maps clicando com botão direito > "O que há aqui?"
+        var map = L.map('mapa-container').setView([-23.5453, -46.6388], 15);
+
+        // 2. Adiciona o desenho do mapa (OpenStreetMap - Gratuito)
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
+
+        // 3. Adiciona o pino (Marker)
+        L.marker([-23.5453, -46.6388]).addTo(map)
+            .bindPopup('<b>Show da Banda Beta</b><br>Sábado às 20h.')
+            .openPopup();
