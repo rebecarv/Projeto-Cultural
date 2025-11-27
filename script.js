576660f1cf863971dcd123ae901bd76935a511fd
rebecarv <saudeefood@gmail.com>
2025-11-27 12:01:20 -0300

    Fix typo in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""// --- CONFIGURAÇÃO DO MAPA ---
+// --- CONFIGURAÇÃO DO MAPA ---
         // 1. Inicializa o mapa nas coordenadas (Latitude, Longitude) e Zoom
         // Exemplo: Coordenadas de um teatro em SP. Você pode pegar no Google Maps clicando com botão direito > "O que há aqui?"
         var map = L.map('mapa-container').setView([-23.5453, -46.6388], 15);
