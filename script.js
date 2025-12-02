c879f91e3c9760cc65692f94a7953a35b3fded97
rebecarv <saudeefood@gmail.com>
2025-12-02 12:04:34 -0300

    Config mapa

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 
-// --- CONFIGURAÇÃO DO MAPA ---
-        // 1. Inicializa o mapa nas coordenadas (Latitude, Longitude) e Zoom
-        // Exemplo: Coordenadas de um teatro em SP. Você pode pegar no Google Maps clicando com botão direito > "O que há aqui?"
-        var map = L.map('mapa-container').setView([-23.5453, -46.6388], 15);
+// --- CONFIGURAÇÃO DO MAPA (Visual Dark) ---
+        
+        // Coordenadas de exemplo em SP (Praça Carlos Gomes, perto de locais alternativos)
+        var latitude = -23.5509;
+        var longitude = -46.6343;
 
-        // 2. Adiciona o desenho do mapa (OpenStreetMap - Gratuito)
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; OpenStreetMap contributors'
+        var map = L.map('mapa-container').setView([latitude, longitude], 14);
+
+        // !!! A MUDANÇA PRINCIPAL NO MAPA ESTÁ AQUI !!!
+        // Em vez do OpenStreetMap padrão (claro), usamos o "CartoDB Dark Matter"
+        // que é um mapa gratuito e escuro, perfeito para o tema do site.
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
+            maxZoom: 20
         }).addTo(map);
 
-        // 3. Adiciona o pino (Marker)
-        L.marker([-23.5453, -46.6388]).addTo(map)
-            .bindPopup('<b>Show da Banda Beta</b><br>Sábado às 20h.')
-            .openPopup();
+        // Marcador personalizado (Um círculo ciano em vez do pino azul padrão)
+        var circle = L.circleMarker([latitude, longitude], {
+            color: '#00f7ff', // Cor da borda (Ciano)
+            fillColor: '#00f7ff', // Cor do preenchimento
+            fillOpacity: 0.8,
+            radius: 10
+        }).addTo(map);
+
+        circle.bindPopup('<b>SUPERVÃO AO VIVO</b><br>Cine Joia - 23h').openPopup();
